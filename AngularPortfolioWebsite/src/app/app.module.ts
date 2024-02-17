d67131a01f0c50425431e033e45162c058233913
Andrew Swint <andrewswint1976@gmail.com>
2024-02-16 22:00:55 -0800

    added nav bar

--- AngularPortfolioWebsite/src/app/app.module.ts
+++ AngularPortfolioWebsite/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
