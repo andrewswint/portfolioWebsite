1fd8bafddbfcadb21ff2cebc4fe14a472cf4731a
Andrew Swint <andrewswint1976@gmail.com>
2024-02-17 11:23:15 -0800

    finished routing logic

--- AngularPortfolioWebsite/src/app/contact/contact.component.ts
+++ AngularPortfolioWebsite/src/app/contact/contact.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Andrew Swint - Contact');
+  }
 }
