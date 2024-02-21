705adb3306d823772d57894146d54d7af7b9ee4d
Andrew Swint <andrewswint1976@gmail.com>
2024-02-20 20:43:36 -0800

    passing data between components

--- AngularPortfolioWebsite/src/app/project-card/project-card.component.ts
+++ AngularPortfolioWebsite/src/app/project-card/project-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.css']
+  styleUrls: ['./project-card.component.css'],
 })
 export class ProjectCardComponent {
-
+  @Input() projectName: string = '';
 }
