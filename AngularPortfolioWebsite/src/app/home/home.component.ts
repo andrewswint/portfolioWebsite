80c150d3de065dc04f1f40cdca21cfdc4f341051
Andrew Swint <andrewswint1976@gmail.com>
2024-02-24 13:04:01 -0800

    implementing created service

--- AngularPortfolioWebsite/src/app/home/home.component.ts
+++ AngularPortfolioWebsite/src/app/home/home.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectsService } from '../_services/projects.service';
+import { Project } from '../_models/Project';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  constructor(private titleService: Title) {
+export class HomeComponent implements OnInit {
+  featuredProject = {} as Project;
+
+  constructor(
+    private titleService: Title,
+    private projectService: ProjectsService
+  ) {
     this.titleService.setTitle('Andrew Swint - Home');
+  }
+  ngOnInit(): void {
+    this.featuredProject = this.projectService.GetProjectById(0);
   }
 }
