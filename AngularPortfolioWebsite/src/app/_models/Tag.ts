b858def7f9d0e0d34762dc519c8b68645c747277
Andrew Swint <andrewswint1976@gmail.com>
2024-02-24 12:32:52 -0800

    looping through projects

--- AngularPortfolioWebsite/src/app/_models/Tag.ts
+++ AngularPortfolioWebsite/src/app/_models/Tag.ts
@@ -2,7 +2,7 @@ export class Tag {
   static readonly ANGULAR = new Tag('Angular', 'red');
   static readonly TYPESCRIPT = new Tag('Typescript', 'blue');
   static readonly JAVA = new Tag('Java', 'green');
-  static readonly JAVASCRIPT = new Tag('Javascript', 'yellow');
+  static readonly JAVASCRIPT = new Tag('Javascript', 'purple');
   static readonly MYSQL = new Tag('MySql', 'orange');
 
   private constructor(
